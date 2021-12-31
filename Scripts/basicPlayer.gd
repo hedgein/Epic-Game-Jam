e75a5d6d57b42e11af0e69424cdccb46c328254e
hedgejess <hedgejess@gmail.com>
2021-12-31 13:50:33 -0600

    fiddling with collisions, still colliding with everything

--- Scripts/basicPlayer.gd
+++ Scripts/basicPlayer.gd
@@ -9,7 +9,7 @@ func _physics_process(delta):
     var y_input = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
     var velocity = Vector2(x_input, y_input) * 60
     #move_and_slide(velocity)
-    var collision = move_and_collide(velocity )
-    if collision:
+    var collision = move_and_collide(velocity * speed)
+    if collision.collider == StaticBody2D:
         emit_signal("collided_with_item")
          
