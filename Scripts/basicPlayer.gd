62abc69d4e5ffd1c874d18e6cda56251b8a75a07
hedgejess <hedgejess@gmail.com>
2022-01-05 15:03:54 -0600

    implemented basic climbing? releasing grab is kind of funky tho

--- Scripts/basicPlayer.gd
+++ Scripts/basicPlayer.gd
@@ -9,7 +9,8 @@ export (int) var gravity = 1000
 
 #additional movement parameters
 export (float, 0 , 1.0) var friction = 0.1
-export (float, 0 , 1,0) var acceleration = 0.25
+export (float, 0 , 1.0) var acceleration = 0.25
+export (float, 0 , 1.0) var grab_friction = 1.0
 
 
 var velocity = Vector2.ZERO
@@ -25,11 +26,41 @@ func get_input():
     else:
         velocity.x = lerp(velocity.x, 0, friction)
 
+func get_input_grab():
+    var dir = 0 
+    if Input.is_action_pressed("move_up"):
+        dir -=1
+    if Input.is_action_pressed("move_down"):
+        dir += 1
+    if dir != 0:
+        velocity.y = lerp(velocity.y, dir * (speed / 2), acceleration)
+    else:
+        velocity.y = lerp(velocity.y, 0, grab_friction)
+func get_input_release(normal):
+    var dir = 0
+    #if wall is grabbed to left
+    if normal == Vector2(1,0):
+        dir += 4
+    #if wall is grabbed to right
+    if normal == Vector2(-1, 0):
+        dir -= 4
+    if dir != 0:
+        velocity.x = lerp(velocity.x, dir * speed, acceleration)
+    else:
+        velocity.x = lerp(velocity.x, 0, friction)
+    velocity.y = jump_speed 
 func _physics_process(delta):
-    get_input()
+    if Input.is_action_pressed("grab"):
+        if is_on_wall():
+            get_input_grab()
+    else: 
+        get_input()
     velocity.y += gravity * delta
     velocity = move_and_slide(velocity, Vector2.UP)
-    if Input.is_action_pressed("move_up"):
+    if (Input.is_action_just_released("grab") && Input.is_action_pressed("move_up")):
+        if is_on_wall():
+            get_input_release(get_last_slide_collision().normal)
+    elif Input.is_action_pressed("move_up"):
         if is_on_floor():
             velocity.y = jump_speed
          
